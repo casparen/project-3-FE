d0864232d19c14294dd7bc1e5b3be1a2176a7d01
Caspar Nagel <casparen@me.com>
2016-08-10 15:11:27 -0400

    starting the form

--- project-HBD/src/components/App.js
+++ project-HBD/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Signup from './signup.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-
+        <h2>App</h2>
+        <Signup />
       </div>
     );
   }
---
+++ project-HBD/src/components/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class SignUpForm extends Component {
+  render(){
+    return (
+      <div>formmmmm</div>
+    )
+  }
+}
+
+export default SignUpForm
