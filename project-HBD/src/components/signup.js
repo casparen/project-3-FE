11eb09f72a6f3fec567b8cebecbe5cbe096962ec
Caspar Nagel <casparen@me.com>
2016-08-10 15:30:25 -0400

    working on form

--- project-HBD/src/components/signup.js
+++ project-HBD/src/components/signup.js
@@ -2,9 +2,41 @@ import React, { Component } from 'react'
 
 
 class SignUpForm extends Component {
+constructor(props){
+  super();
+  this.state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    day: "",
+    month:"",
+    year: ""
+  }
+}
+
+
+
+  handleSubmit(event){
+    event.preventDefault()
+    console.log(this.state.Fname);
+  }
   render(){
     return (
-      <div>formmmmm</div>
+      <div>
+        <h1>Sign up</h1>
+        <form>
+          <input type="text" placeholder="first name" onChange={(event) => this.setState({firstName: event.target.value})} value={this.state.firstName}></input>
+          <input type="text" placeholder="last name" onChange={(event) => this.setState({lastName: event.target.value})} value={this.state.lastName}></input>
+          <input type="text" placeholder="email" onChange={(event) => this.setState({email: event.target.value})} value={this.state.email}></input>
+          <div>
+            <input type="text" placeholder="day" onChange={(event) => this.setState({day: event.target.value})} value={this.state.day}></input>
+            <input type="text" placeholder="month" onChange={(event) => this.setState({month: event.target.value})} value={this.state.month}></input>
+            <input type="text" placeholder="year" onChange={(event) => this.setState({year: event.target.value})} value={this.state.year}></input>
+          </div>
+          <button onClick={(event) => this.handleSubmit(event)}>SUBMIT</button>
+        </form>
+    </div>
+
     )
   }
 }
