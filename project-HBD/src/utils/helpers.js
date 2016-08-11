0e043177bf69c27d4ce5ae950b1afb74f5ac1b5e
Caspar Nagel <casparen@me.com>
2016-08-11 13:51:03 -0400

    setting up firebase

--- project-HBD/src/utils/helpers.js
+++ project-HBD/src/utils/helpers.js
@@ -6,5 +6,25 @@ export default {
     return fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy birthday' + tag, fetchSettings).then((response) => {
       return response.json();
     })
+  },
+
+  addToDB: function (data){
+    console.log("helper", data);
+    const fetchSettings = {
+      method: 'POST',
+      body: JSON.stringify(data),
+
+    return fetch('https://happybday-d595a.firebaseio.com/wdi/robots/users.json', fetchSettings).then((response) => {
+      return response.json();
+    })
+  },
+
+  checkForMatch: function (email){
+    const fetchSettings = {
+      method: 'GET'
+    }
+    return fetch('https://happybday-d595a.firebaseio.com/wdi/robots/users' + email + 'json', fetchSettings).then((response) => {
+      return response.json();
+    })
   }
 }
