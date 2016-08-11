9cfd1ac519e8e123c9d12764b425de01858d658d
Caspar Nagel <casparen@me.com>
2016-08-11 17:36:28 -0400

    working on log in

--- project-HBD/src/components/App.js
+++ project-HBD/src/components/App.js
@@ -8,16 +8,12 @@ class App extends Component {
     render() {
         return (
             <div>
-                <navigation>
-                    <Link to='/profile'>
-                        <button>Your profile</button>
-                    </Link>
-                    <Link to='/allProfiles'>
-                        <button>View all</button>
-                    </Link>
-                </navigation>
-                <h4>I am App</h4>
-                {this.props.children}
+              <navigation>
+                  <Link to='/profile'><button>Your profile</button></Link>
+                  <Link to='/allProfiles'><button>View all</button></Link>
+              </navigation>
+              <h4>I am App</h4>
+              {this.props.children}
             </div>
         )
     }
