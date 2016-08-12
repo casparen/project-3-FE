ffdf8b81cccfda8a863e25016ece4515c89c6bd6
Harry Hur <sungju1203@hotmail.com>
2016-08-12 12:44:58 -0400

    dashboard

--- project-HBD/src/components/Dashboard.js
+++ project-HBD/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import helpers from '../utils/helpers';
+import DashboardList from './DashboardList';
 import _ from 'lodash';
 import {Link} from 'react-router'
 
@@ -12,23 +13,26 @@ class Dashboard extends Component {
         helpers.checkForMatch()
             .then(res => {
                 let user = _.values(res).filter(each => each);
-                console.log(user);
-                this.setState({response: user})
+                this.setState({response: user});
+                console.log("state: ", this.state.response);
             })
     }
 
     render() {
+        if (this.state.response.length === 0) {
+            return <div>LOADING...</div>
+        }
         return (
             <div>
-                <h1>View all profiles</h1>
-                <ul>
-                </ul>
-                <Link to='/message'>
-                    <button>Link to message</button>
-                </Link>
+                <h3>DASHBOARD</h3>
+                <DashboardList users={this.state.response} />
             </div>
         )
     }
 }
 
 export default Dashboard
+
+// <Link to='/message'>
+//     <button>Link to message</button>
+// </Link>
