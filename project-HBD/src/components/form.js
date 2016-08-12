c72d9260879c9e2d4f636b13d91c099b44574b51
Alberto Gomez <01@gom3z.com>
2016-08-12 13:09:10 -0400

    udpates to import from react routes etc.

--- project-HBD/src/components/form.js
+++ project-HBD/src/components/form.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../styles/form.css';
 import Helper from '../utils/helpers.js';
-import {Link} from 'react-router';
+import {Link, Router, Route, browserHistory} from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 
@@ -26,11 +26,6 @@ class SignUpForm extends Component {
           this.setState({fbUser: res})
         })
     }
-
-    birthday() {
-
-    }
-
 
     handleSubmit(event) {
 
@@ -61,6 +56,8 @@ class SignUpForm extends Component {
             Helper.addToDB(data).then((res) => {
               console.log(res);
             })
+            return browserHistory.push('/app')
+
 
 
     }
@@ -115,7 +112,7 @@ class SignUpForm extends Component {
                         <option value="robots">robots</option>
                         <option value="purple rain">purple rain</option>
                     </select>
-                      <button onClick={(event) => this.handleSubmit(event)}><Link to='/app'>SUBMIT</Link></button>
+                      <button onClick={(event) => this.handleSubmit(event)}></button>
                 </form>
             </div>
 
