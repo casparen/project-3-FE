0c5991c0e8a7d73266f9d89e483159defd62e186
Harry Hur <sungju1203@hotmail.com>
2016-08-13 18:18:58 -0400

    c

--- project-HBD/src/utils/helpers.js
+++ project-HBD/src/utils/helpers.js
@@ -1,13 +1,17 @@
 const Helpers = {
     getGiphy: (tag) => {
         return fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy%20birthday')
-            .then((response) => response.json())
+            .then(res => res.json())
     },
     getCurrentUser: (uid) => {
         const fetchSettings = {method: 'GET'};
         return fetch(`https://happybday-d595a.firebaseio.com/ga/wdi/robots/users/${uid}.json`, fetchSettings)
-            .then(response => response.json())
+            .then(res => res.json())
     },
+    getUserGroup: () => {
+        return fetch("https://happybday-d595a.firebaseio.com/ga/wdi/robots/users.json")
+            .then(res => res.json())
+    }
 };
 
 module.exports = Helpers;
