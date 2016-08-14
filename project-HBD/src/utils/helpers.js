4faa5ba8403e037883b40832a62fbade683ad645
Harry Hur <sungju1203@hotmail.com>
2016-08-14 16:28:26 -0400

    changes

--- project-HBD/src/utils/helpers.js
+++ project-HBD/src/utils/helpers.js
@@ -1,8 +1,10 @@
 const Helpers = {
+    // to giphy server
     getGiphy: (tag) => {
         return fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy%20birthday')
             .then(res => res.json())
     },
+    // to firebase
     getCurrentUser: (uid) => {
         const fetchSettings = {method: 'GET'};
         return fetch(`https://happybday-d595a.firebaseio.com/ga/wdi/robots/users/${uid}.json`, fetchSettings)
@@ -16,7 +18,19 @@ const Helpers = {
     getUserGroup: () => {
         return fetch("https://happybday-d595a.firebaseio.com/ga/wdi/robots/users.json")
             .then(res => res.json())
+    },
+    // to backend server
+    sendMessage: (data) => {
+        const fetchSettings = {
+            method: "POST",
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify(data)
+        };
+        return fetch("http://immense-garden-30944.herokuapp.com/message", fetchSettings)
     }
+
 };
 
 module.exports = Helpers;
