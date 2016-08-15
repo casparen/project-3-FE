f861fbbce66bfa94b6b8dc4f1c0db128bebb8702
Harry Hur <sungju1203@hotmail.com>
2016-08-15 00:27:30 -0400

    changes

--- project-HBD/src/components/DashboardListItem.js
+++ project-HBD/src/components/DashboardListItem.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Button, Panel} from 'react-bootstrap';
 import '../styles/dashboard.css';
-import { Button } from 'react-bootstrap';
 
 const DashboardListItem = ({user}) => {
     // const user = this.props.user;
     return (
         <li className="profileStats">
-            <div><img className="default" src="http://hackersworld.org/images/profile.png" role="presentation"/></div>
-            <div>
-                Name: {user.name} 
-                Birthday: {user.dob}
-                Mobile Number: {user.phone}
-                email: {user.email}
-            </div>
-            <div>
-                <Link to={{pathname:`/message/${user.uid}`}}>
-                    <Button bsStyle="warning">Send a message</Button>
-                </Link>
-            </div>
+            <Panel header={user.name} bsStyle="success">
+                <div className="avatarContainer">
+                    <img className="default" src="http://hackersworld.org/images/profile.png" role="presentation"/>
+                </div>
+                <div className="userInfoContainer">
+                    Name: {user.name}
+                    Birthday: {user.dob}
+                    Mobile Number: {user.phone}
+                    email: {user.email}
+                </div>
+                <div className="">
+                    <Link to={{pathname:`/message/${user.uid}`}}>
+                        <Button bsStyle="warning">Send a message</Button>
+                    </Link>
+                </div>
+            </Panel>
+
         </li>
     )
 };
